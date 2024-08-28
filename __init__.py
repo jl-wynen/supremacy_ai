188da27888ba08648e2e0cdecfee574bc58611f7
Neil Vaytet <neil.vaytet@ess.eu>
2024-08-28 19:40:02 +0200

    add barebones ai

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
-from .bot import CREATOR, PlayerAi
+from .simple_ai import CREATOR, PlayerAi
+# from .barebones_ai import CREATOR, PlayerAi
 
 __all__ = ["CREATOR", "PlayerAi"]
